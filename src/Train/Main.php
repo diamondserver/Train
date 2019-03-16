58aef555f0f614736ea6c66950a8f2652f49b94c
R_Funa <funaha0318@icloud.com>
2019-03-16 16:45:35 +0900

    Base

---
+++ src/Train/Main.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Train;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\math\Vector3;
+use pocketmine\level\Position;
+
+class Main extends PluginBase implements Listener{
+
+	public function onEnable()
+	{
+     $this->getServer()->getPluginManager()->registerEvents($this,$this);
+	}
+   
+}
